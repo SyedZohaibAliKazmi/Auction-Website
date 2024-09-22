893f32d55ebdef4ea94a6c212ab425af37160461
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-09-22 22:29:00 +0500

    'Auction-Website

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 
 function App() {
 
+  const handleClick =()=>{
+    window.open('https://github.com/SyedZohaibAliKazmi', '_blank');
+  }
+
   return (
     <>
     <h1>Auction Website </h1>
+    <p>Craete a Auction Website for new Versio.</p>
+    <button onClick={handleClick}>Click</button>
     </>
   )
 }
