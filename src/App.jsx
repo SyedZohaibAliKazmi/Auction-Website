5d1e876c095fbd1da07a1c79cda3e23063edcc6c
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-09-26 17:53:59 +0500

    Auction-Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Products from './Pages/Products/Products'
 import Navbar from './Components/Navbar/Navbar'
-
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Contact from './Pages/Contact/Contact'
+import SignIn from './Pages/Auth/SignIn'
 function App() {
 
 
  return(
   <BrowserRouter>
   <Navbar/>
+
   <Routes>
+    {/* <Route path='/' element={<Home />} />  Default path */}
+    <Route path='/Home' element={<Home/>}/>
+    <Route path='/Products' element={<Products/>}/>
+    <Route path='About' element={<About/>}/>
+    <Route path='Contact' element={<Contact/>}/>
+    <Route path='SignIn' element={<SignIn/>}/>
     
   </Routes>
   </BrowserRouter>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,62 @@
-
+import { useState,useRef,useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { gsap } from "gsap";
+import { faUser, faCartShopping, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-    
+  const menuRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  
 
-    return(
-
-        <h1 style={{background:"black", color:"white", borderRadius:"50px", width:"90%", height:"60px",display:"flex", alignItems:"center", justifyContent:"center"}}>Navbar</h1>
-
-    )
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  useEffect(() => {
+    if (isOpen) {
+      gsap.to(menuRef.current, {
+        duration: 0.5,
+        ease: "power3.out",
+      });
+      gsap.from(menuRef.current, {
+        duration: 0.5,
+        ease: "power3.out",
+        x: "100%",
+      })}else {
+        gsap.to(menuRef.current, {
+          duration: 0.5,
+          ease: "power3.in",
+        });
+      }
+    }, [isOpen]);
+  return (
+    <div>
+      <div className="main-navbar">
+        <div className="logo">
+          <h1>Logo</h1>
+        </div>
+        <div  ref={menuRef} className={`pages-link ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/Products">Products</Link></li>
+            <li><Link to="/About">About</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className="Navbar-Account">
+          <Link to="./Signin">
+          <button className="account-button">
+            <FontAwesomeIcon icon={faUser} />
+          </button>
+          </Link>
+        </div>
+        <button className="hamburger" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
