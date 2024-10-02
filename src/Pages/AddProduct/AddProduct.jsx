a8f37ccbdb7ca3992525911a63342aa336a3b6cc
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-10-02 20:47:41 +0500

    Biding-Website

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -3,20 +3,62 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import Input from "../../Components/Input/Input";
 import './AddProduct.css'
+import { message } from "antd";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { auth, db, storageDB } from "../../utils/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 function AddProduct() {
   const navigate = useNavigate()
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
 
-    const onSubmit = (data) => {
-        console.log("hoa=>", data);
+
+    
+    const onSubmit = async (data) => {
+
+try {
+
+  if (!data.img || data.img.length === 0) {
+    message.error("Please upload an image.");
+    return; // Stop the function if no file is selected
+}
+
+  const file = data.img[0];
+ 
+  const storageRef = ref(storageDB,`products/${file.name}`)
+  const snapshot = await uploadBytes(storageRef,file)
+  const downloadURL  = await  getDownloadURL(snapshot.ref)
+
+
+ // Step 2: Add document to Firestore
+
+      const productCollectionRef = collection(db,"products")
+      console.log("data",data)
+      const obj={
+        ...data,
+        img: downloadURL,
+        createdAt:serverTimestamp(),
+        createdBy: auth.currentUser.uid,
+        status: "active",
+      }
+     await addDoc(productCollectionRef,obj)
+        reset()
+        message.success("Product added successfully!")
+  
+  
+} catch (error) {
+  console.log("error=>",error);
+  message.error("Error in adding product!")
+  
+  
+}
+   
         
     }
 
     return(
         <>
-        {/* <h1>helllo</h1> */}
 
         <form onSubmit={handleSubmit(onSubmit)}>
 
